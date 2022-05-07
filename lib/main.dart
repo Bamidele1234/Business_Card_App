abd001a9ba2d098ce536000900d51028aedb212a
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-05-07 17:41:12 +0100

    Completed the classwork: Flutter layouts challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const PracticeApp());
 
 // Using Row and Column
 class MyApp extends StatelessWidget {
@@ -95,6 +95,51 @@ class SomeApp extends StatelessWidget {
              * Enable rotation, similar to that of Processing
              */
             transform: Matrix4.rotationZ(0.2),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Classwork / Exercise
+class PracticeApp extends StatelessWidget {
+  const PracticeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                width: 100,
+                color: Colors.blue,
+              ),
+            ],
           ),
         ),
       ),
