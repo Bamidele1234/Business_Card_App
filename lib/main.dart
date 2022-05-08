958a464b4749755e621f54aa30648eb90af3c2d6
Bamidele Ajewole <bamideledavid.femi@gmail.com>
2022-05-08 15:17:32 +0100

    Finished the Business Card App

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,89 @@ class FinalApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.teal.shade300,
+          title: const Text('Business Card'),
+        ),
         body: SafeArea(
           child: Column(
-            children: const <Widget>[
-              CircleAvatar(
-                radius: 50,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                radius: 65,
                 backgroundImage: AssetImage('images/me.png'),
               ),
-              Text(
-                'Bamidele',
+              const Text(
+                'Ajewole Bamidele',
                 style: TextStyle(
+                  fontFamily: 'Poppins',
                   color: Colors.white,
                   fontSize: 40,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Mobile App Developer',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(3),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+234 8102 666 712',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(3),
+                  child: ListTile(
+                    leading: const Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'bamideledavid.ajewole@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
